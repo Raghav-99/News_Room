57ddbd77ca6b6a6775e6b238ebabbe253f6c7bb4
Raghav-99 <raghavmewar2000@gmail.com>
2022-09-18 17:35:02 +0530

    fixed form submission with client side validation

--- JS/login.js
+++ JS/login.js
@@ -1,12 +1,12 @@
 $('document').ready(() => {
-    // setup a click handler on Login button
-    $('#login').click((event) => {
+    // setup a submit handler on form
+        $('#form_login').submit((event) => {
         event.preventDefault();         // preventDefault() when invoked will prevent default actions
                                         // to be executed. In this case it will prevent the form from
                                         // submitting in default manner
                                         // ref: https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
         
-        location.href = 'main.html';    // will route to home page(acts like the <a> tag)
+        window.location.replace('main.html');    // will route to home page(acts like the <a> tag)
         return false;
-    });
+    })
 })
