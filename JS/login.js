ca830596b6bb6262c6f296643f94485ef554f7bf
Raghav-99 <raghavmewar2000@gmail.com>
2022-09-18 16:03:37 +0530

    set up login redirection

--- JS/login.js
+++ JS/login.js
@@ -1,7 +1,12 @@
 $('document').ready(() => {
-    $('login').click((event) => {
-        event.preventDefault();
-        location.href = 'main.html';
+    // setup a click handler on Login button
+    $('#login').click((event) => {
+        event.preventDefault();         // preventDefault() when invoked will prevent default actions
+                                        // to be executed. In this case it will prevent the form from
+                                        // submitting in default manner
+                                        // ref: https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+        
+        location.href = 'main.html';    // will route to home page(acts like the <a> tag)
         return false;
     });
 })
