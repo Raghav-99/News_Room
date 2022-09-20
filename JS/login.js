7005a024178331257a8f02e6fedec39b2245a1e6
Raghav-99 <raghavmewar2000@gmail.com>
2022-09-21 02:00:03 +0530

    working on weather and date time API

--- JS/login.js
+++ JS/login.js
@@ -1,6 +1,23 @@
+let query_string = "Hyderabad,IN";
+let api_key = "143b7c3353d5d68c3ed01f4f30327d93"
+let weather_data = `https://api.openweathermap.org/data/2.5/weather?q=${query_string}&appid=${api_key}&units="metric"`
+
 $('document').ready(() => {
+    // Weather API call here
+    $.ajax({
+        url: weather_data,
+        method: 'GET',
+        success: (data) => {
+            //console.log(data);
+            
+        },
+        error: () => {
+            console.log('Error occurred');
+        },
+    })
+
     // setup a submit handler on form
-        $('#form_login').submit((event) => {
+    $('#form_login').submit((event) => {
         event.preventDefault();         // preventDefault() when invoked will prevent default actions
                                         // to be executed. In this case it will prevent the form from
                                         // submitting in default manner
