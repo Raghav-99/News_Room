bfd3f6c4392f7bce95111406a99b70a34cf826c3
Raghav-99 <raghavmewar2000@gmail.com>
2022-09-22 00:58:28 +0530

    shifted API calls to a dedicated script api.js

--- JS/login.js
+++ JS/login.js
@@ -1,30 +1,4 @@
-let query_string = "Hyderabad,IN";
-let api_key = "143b7c3353d5d68c3ed01f4f30327d93"
-let weather_data = `https://api.openweathermap.org/data/2.5/weather?q=${query_string}&appid=${api_key}&units=metric`
-
 $('document').ready(() => {
-    //* Weather API call here
-    var weather_icon;
-    $.ajax({
-        url: weather_data,
-        method: 'GET',
-        success: (data) => {
-            // console.log(data);
-
-            //* dynamically fetch weather icon according to weather
-            weather_icon = "http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png";
-            //console.log(weather_icon);
-
-            //* insert weather icon
-            $('#w_icon').attr('src',weather_icon); 
-            //* insert weather description and temp data
-            $('#w_desc').html(data.weather[0].description+'<br>'+data.main.temp);
-        },
-        error: () => {
-            console.log('Error occurred');
-        },
-    })
-
     // setup a submit handler on form
     // TODO: set up auth handling by Oshin(remove comment once done)
     $('#form_login').submit((event) => {
